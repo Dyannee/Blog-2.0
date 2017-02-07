2d27ec9f3b13854eae13b9069fac2b2d4a7c9e37
Dyannee <Dyanne0033@gmail.com>
2017-02-07 18:24:57 -0300

    Commited

---
+++ try/goron/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  root 'homepage#index'
+  resources :users
+  resources :albums do
+    resources :comments
+  end
+
+  get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
